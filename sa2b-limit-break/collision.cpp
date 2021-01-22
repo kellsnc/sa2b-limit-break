c35169801c33756b823fe551a1ddf4ee1abe2a4d
kellsnc <kellspam@outlook.fr>
2021-01-23 00:41:17 +0100

    Dynamic object collisions count (part 2)

--- sa2b-limit-break/collision.cpp
+++ sa2b-limit-break/collision.cpp
@@ -19,6 +19,7 @@ namespace CollisionList
 }
 
 static std::vector<ObjectMaster*> entities[CollisionList::COUNT] = {};
+static std::vector<ObjectMaster*> old_entities[CollisionList::COUNT] = {};
 
 static void __cdecl AddToCollisionList_r(ObjectMaster* obj)
 {
@@ -192,16 +193,6 @@ static void __cdecl RunChaoCollision_r() {
 static void __cdecl RunEnemyCollision_r() {
 	CheckSelfCollision(3);
 
-	if (entities[2].size() > 0) {
-		WriteData((ObjectMaster***)0x4C4ADD, &entities[2][0]);
-		WriteData((ObjectMaster***)0x74A25D, &entities[2][0]);
-	}
-
-	if (entities[3].size() > 0) {
-		WriteData((ObjectMaster***)0x4C4AF7, &entities[3][0]);
-		WriteData((ObjectMaster***)0x74A266, &entities[3][0]);
-	}
-	
 	for (auto& i : entities[3])
 	{
 		for (auto& x : entities[4])
@@ -239,12 +230,15 @@ static void __cdecl ClearCollisionLists_r()
 
 	for (size_t i = 0; i < CollisionList::COUNT; i++)
 	{
+		old_entities[i].clear();
+		old_entities[i].reserve(entities->size());
+
+		for (auto& j : entities[i]) {
+			old_entities[i].push_back(j);
+		}
+
 		entities[i].clear();
 	}
-}
-
-ObjectMaster* GetClosestCollisionDyn(int index, CharObj2Base* co2, float* dist) {
-
 }
 
 static void __cdecl ScanMechTargets_r(CharObj2Base* co2, MechEggmanCharObj2* eggco2) {
@@ -252,10 +246,10 @@ static void __cdecl ScanMechTargets_r(CharObj2Base* co2, MechEggmanCharObj2* egg
 		float dist_target = 0;
 		float dist_temp_target = 0;
 
-		ObjectMaster* target = GetClosestCollisionDyn(2, co2, &dist_target);
-		ObjectMaster* temp_target = GetClosestCollisionDyn(3, co2, &dist_temp_target);
-		ObjectMaster* target_player = MainCharacter[1];
-
+		// Get objects that intersect with the mech ray
+		ObjectMaster* target = GetTargetCollision(TargetCollisions_LastCount, co2, old_entities[CollisionList::Targetable].data(), &dist_target);
+		ObjectMaster* temp_target = GetTargetCollision(EnemiesCollisions_LastCount, co2, old_entities[CollisionList::Enemies].data(), &dist_temp_target);
+		
 		if (target) {
 			if (temp_target && dist_temp_target < dist_target) {
 				dist_target = dist_temp_target;
@@ -267,18 +261,60 @@ static void __cdecl ScanMechTargets_r(CharObj2Base* co2, MechEggmanCharObj2* egg
 			target = temp_target;
 		}
 
+		// Get players that intersect with the mech ray
+		ObjectMaster* target_player = MainCharacter[1];
+
 		if (co2->PlayerNum) {
 			target_player = MainCharacter[0];
 		}
 		if (target_player) {
-			ObjectMaster* playerobj = GetClosestPlayer(target_player, co2, &dist_temp_target);
+			ObjectMaster* playerobj = GetTargetPlayer(target_player, co2, &dist_temp_target);
 
-			if (*&dist_temp_target > 0.0 && *&dist_target > *&dist_temp_target) {
+			if (dist_temp_target > 0.0 && dist_target > dist_temp_target) {
 				target = playerobj;
 			}
 		}
 
-		// todo : continue
+		// Target acquired
+		if (target) {
+			EntityData1* target_data = target->Data1.Entity;
+
+			if (!target_data || (target_data->field_2 != 22 && target_data->field_2 != 23)) {
+				ObjectMaster* targettask = LoadChildObject((LoadObj)(LoadObj_Data1), (ObjectFuncPtr)0x74C260, MainCharacter[co2->PlayerNum]);
+				
+				if (targettask) {
+					TargetThing* targetinfo = new TargetThing();
+
+					targettask->Data2.Undefined = (void*)targetinfo;
+
+					targetinfo->playernum = co2->PlayerNum;
+					targetinfo->target = target;
+
+					sub_47EEC0(target_data, target_data->Collision->CollisionArray, &targetinfo->position);
+					sub_74AE30(eggco2, targettask);
+
+					if (GetObjectColList(target)) {
+						target->EntityData2->field_10 = *(float*)targettask;
+					}
+					else {
+						*(&target->Data2.Undefined + 227) = (void*)targettask;
+					}
+
+					eggco2->field_368 += 1;
+					eggco2->field_35E = 1;
+					eggco2->field_390 = targetinfo->position.x;
+					eggco2->field_394 = targetinfo->position.y;
+					eggco2->field_398 = targetinfo->position.z;
+					eggco2->field_366 = 7;
+					targettask->field_1C = (ObjectFuncPtr)0x74BEC0;
+					targettask->DeleteSub = (ObjectFuncPtr)0x74BDB0;
+					PlaySoundProbably(8207, 0, 0, 0);
+				}
+				else {
+					DeleteObject_(targettask);
+				}
+			}
+		}
 	}
 }
 
@@ -288,11 +324,143 @@ static void __declspec(naked) ScanMechTargets_asm()
 	{
 		push[esp + 04h] // eggco2
 		push eax // co2
-		call sub_74CCF0
+		call ScanMechTargets_r
 		pop eax
 		add esp, 4
 		retn
 	}
+}
+
+signed int sub_486D60(int a1)
+{
+	if (!a1) {
+		return TargetCollisions_LastCount;
+	}
+	else if (a1 == 1) {
+		return EnemiesCollisions_LastCount;
+	}
+
+	return -1;
+}
+
+bool sub_74A140(EntityData1* data, MechEggmanCharObj2* eggco2) {
+	int v2; // ecx
+	char v3; // al
+	signed int v4; // eax
+	MechEggmanCharObj2* v5; // eax
+	Float v6; // ecx
+	Float v7; // edx
+	Float v8; // eax
+	int v10; // ecx
+	ObjectMaster** v11; // ebx
+	int v12; // eax
+	signed int v13 = 0; // ecx
+	CollisionInfo* v14; // ecx
+	ObjectMaster* v15; // eax
+	EntityData1* v16; // eax
+	CollisionData* v17; // ecx
+	signed int v18; // [esp+14h] [ebp-20h]
+	signed int v19; // [esp+18h] [ebp-1Ch]
+	NJS_VECTOR vec1; // [esp+1Ch] [ebp-18h]
+	NJS_VECTOR vec2; // [esp+28h] [ebp-Ch]
+
+	if (data->Action && data->Action != 1 && data->Action != 17) {
+		return 0;
+	}
+	
+	int idk = sub_46DE60(0, eggco2->base.PlayerNum);
+
+	if (idk == -1) {
+		goto LABEL_23;
+	}
+
+	v5 = (MechEggmanCharObj2*)MainCharObj2[idk];
+	vec1 = *(NJS_VECTOR*)v5->field_1BC[92];
+	vec2 = { 1, 0, 0 };
+	
+	CalcVector_PlayerRot(data, &vec2);
+
+	vec2 = { vec2.x * 20.0f, vec2.y * 20.0f, vec2.z * 20.0f };
+	
+	vec1.x = (v5->field_1BC[92] - v5->field_1BC[92]) - vec2.x;
+	vec1.y = (v5->field_1BC[96] - v5->field_1BC[96]) - vec2.y;
+	vec1.z = (v5->field_1BC[100] - v5->field_1BC[100]) - vec2.z;
+
+	if (njScalor(&vec1) >= 15.0f)
+	{
+	LABEL_23:
+		v10 = 2;
+		v19 = 2;
+		while (1)
+		{
+			if (v10 == 2)
+			{
+				v11 = old_entities[CollisionList::Targetable].data();
+				v12 = 0;
+			}
+			else
+			{
+				v11 = old_entities[CollisionList::Enemies].data();
+				v12 = 1;
+			}
+			v18 = sub_486D60(v12);
+			if (v18 > 0)
+			{
+				break;
+			}
+		LABEL_19:
+			v10 = v13 - 1;
+			v19 = v10;
+			if (v10 <= 0)
+			{
+				return 0;
+			}
+		}
+		while (1)
+		{
+			v14 = v11[0]->Data1.Entity->Collision;
+			++v11;
+			if (v14)
+			{
+				v15 = v14->Object;
+				if (v15)
+				{
+					v16 = v15->Data1.Entity;
+					if (v16)
+					{
+						v17 = v14->CollisionArray;
+						if (v17->kind != 12)
+						{
+							sub_47EEC0(v16, v17, &vec1);
+							vec2.x = 1.0;
+							vec2.z = 0.0;
+							vec2.y = 0.0;
+							CalcVector_PlayerRot(data, &vec2);
+							vec2.x = vec2.x * 30.0;
+							vec2.y = vec2.y * 30.0;
+							vec2.z = 30.0 * vec2.z;
+							vec1.x = vec1.x - *&eggco2->field_1BC[92];
+							vec1.y = vec1.y - *&eggco2->field_1BC[96];
+							vec1.z = vec1.z - *&eggco2->field_1BC[100];
+							vec1.x = vec1.x - vec2.x;
+							vec1.y = vec1.y - vec2.y;
+							vec1.z = vec1.z - vec2.z;
+							if (njScalor(&vec1) < 15.0)
+							{
+								break;
+							}
+						}
+					}
+				}
+			}
+			if (--v18 <= 0)
+			{
+				v13 = v19;
+				goto LABEL_19;
+			}
+		}
+	}
+	return 1;
 }
 
 void Collision_Init() {
@@ -305,4 +473,7 @@ void Collision_Init() {
 	WriteJump((void*)0x485F50, RunCollision_5_r);
 	WriteJump((void*)0x485FD0, ClearCollisionLists_r);
 	WriteJump((void*)0x74CCF0, ScanMechTargets_asm);
+	WriteJump((void*)0x74A140, sub_74A140);
+
+	WriteData<5>((void*)0x74BE8B, 0x90);
 }
--- sa2b-limit-break/pointers.h
+++ sa2b-limit-break/pointers.h
@@ -8,6 +8,7 @@ VoidFunc(RunEnemyCollision, 0x485E10);
 VoidFunc(RunRegularCollision, 0x485EF0);
 
 FunctionPointer(void, CheckCollision, (ObjectMaster* entity, ObjectMaster* test), 0x485850);
+FastcallFunctionPointer(signed int, sub_46DE60, (int a1, int a2), 0x46DE60);
 
 DataArray(ObjectMaster*, CollisionList_1_List, 0x19466A0, 128);
 DataArray(ObjectMaster*, CollisionList_2_List, 0x1DE4EE0, 128);
@@ -33,6 +34,18 @@ DataPointer(unsigned __int16, CollisionList_9_Count, 0x1946ABC);
 DataPointer(unsigned __int16, TargetCollisions_LastCount, 0x1DE4EC8);
 DataPointer(unsigned __int16, EnemiesCollisions_LastCount, 0x1DE944A);
 
+struct TargetThing
+{
+	int unk0;
+	int unk4;
+	__int8 playernum;
+	__int8 unk9;
+	__int16 unkA;
+	int unkC;
+	NJS_VECTOR position;
+	ObjectMaster* target;
+};
+
 //void __usercall Collision_InitThings(ObjectMaster *obj@<eax>)
 static const void* const Collision_InitThingsPtr = (void*)0x47E6C0;
 static inline void Collision_InitThings(ObjectMaster* obj)
@@ -45,8 +58,8 @@ static inline void Collision_InitThings(ObjectMaster* obj)
 }
 
 //ObjectMaster *__usercall GetClosestPlayer@<eax>(ObjectMaster *player@<ebx>, CharObj2Base *co2@<esi>, float *dist_output)
-static const void* const GetClosestPlayerPtr = (void*)0x74CC40;
-static inline ObjectMaster* GetClosestPlayer(ObjectMaster* player, CharObj2Base* co2, float* dist_output)
+static const void* const GetTargetPlayerPtr = (void*)0x74CC40;
+static inline ObjectMaster* GetTargetPlayer(ObjectMaster* player, CharObj2Base* co2, float* dist_output)
 {
 	ObjectMaster* result;
 	__asm
@@ -54,9 +67,67 @@ static inline ObjectMaster* GetClosestPlayer(ObjectMaster* player, CharObj2Base*
 		push[dist_output]
 		mov esi, [co2]
 		mov ebx, [player]
-		call GetClosestPlayerPtr
+		call GetTargetPlayerPtr
 		mov result, eax
 		add esp, 4
 	}
 	return result;
+}
+
+//ObjectMaster *__usercall GetTargetCollision@<eax>(int colcount@<eax>, CharObj2Base *co2@<edx>, ObjectMaster **collist@<ecx>, float *dist)
+static const void* const GetTargetCollisionPtr = (void*)0x74CB30;
+static inline ObjectMaster* GetTargetCollision(int colcount, CharObj2Base* co2, ObjectMaster** collist, float* dist_output)
+{
+	ObjectMaster* result;
+	__asm
+	{
+		push[dist_output]
+		mov ecx, [collist]
+		mov edx, [co2]
+		mov eax, [colcount]
+		call GetTargetCollisionPtr
+		mov result, eax
+		add esp, 4
+	}
+	return result;
+}
+
+//void __usercall sub_47EEC0(EntityData1 *data@<eax>, CollisionData *col@<edi>, NJS_VECTOR *out_vec)
+static const void* const sub_47EEC0Ptr = (void*)0x47EEC0;
+static inline void sub_47EEC0(EntityData1* data, CollisionData* col, NJS_VECTOR* out_vec)
+{
+	__asm
+	{
+		push[out_vec]
+		mov edi, [col]
+		mov eax, [data]
+		call sub_47EEC0Ptr
+		add esp, 4
+	}
+}
+
+//signed int __usercall GetObjectColList@<eax>(ObjectMaster *obj@<eax>)
+static const void* const GetObjectColListPtr = (void*)0x486CB0;
+static inline signed int GetObjectColList(ObjectMaster* obj)
+{
+	signed int result;
+	__asm
+	{
+		mov eax, [obj]
+		call GetObjectColListPtr
+		mov result, eax
+	}
+	return result;
+}
+
+//void __usercall sub_74AE30(MechEggmanCharObj2 *eggco2@<ecx>, ObjectMaster *obj@<esi>)
+static const void* const sub_74AE30Ptr = (void*)0x74AE30;
+static inline void sub_74AE30(MechEggmanCharObj2* eggco2, ObjectMaster* obj)
+{
+	__asm
+	{
+		mov esi, [obj]
+		mov ecx, [eggco2]
+		call sub_74AE30Ptr
+	}
 }
