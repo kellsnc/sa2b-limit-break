dffa99d0cc979880d6bed2590ddb9d34036ccff5
kellsnc <kellspam@outlook.fr>
2021-07-14 05:47:41 +0200

    Add set list expansion, fix Taimatu fix

--- sa2b-limit-break/objects.cpp
+++ sa2b-limit-break/objects.cpp
@@ -3,7 +3,9 @@
 #define TRAMPOLINE(name) ((decltype(name##_r)*)name##_t->Target())
 
 static constexpr int MaxPlayers = 2;
+static constexpr uint32_t SETCount = 4096;
 
+static SETObjectData SetTable[SETCount]{};
 static Trampoline* TaimatuExec_t = nullptr;
 
 static float GetDistance(NJS_VECTOR* orig, NJS_VECTOR* dest)
@@ -13,6 +15,11 @@ static float GetDistance(NJS_VECTOR* orig, NJS_VECTOR* dest)
 
 static bool isPlayerAround(NJS_VECTOR* position, float radius)
 {
+	if (CameraData1 && GetDistance(position, &CameraData1->Position) < radius)
+	{
+		return true;
+	}
+
 	for (int i = 0; i < MaxPlayers; ++i)
 	{
 		if (MainCharObj1[i])
@@ -33,9 +40,15 @@ static void __cdecl TaimatuExec_r(ObjectMaster* obj)
 
 	EntityData1* data = obj->Data1.Entity;
 
-	if (isPlayerAround(&data->Position, 200.0f) == true)
+	if (isPlayerAround(&data->Position, 300.0f) == true)
 	{
 		Play3DSound_EntityAndPos(data, 4, &data->Position, 0);
+		data->Index = 1;
+	}
+	else if (data->Index == 1)
+	{
+		DeleteSoundsOfObject(data);
+		data->Index = 0;
 	}
 }
 
@@ -46,5 +59,24 @@ void Objects_Init(const IniFile* config)
 		// Fix Taimatu
 		TaimatuExec_t = new Trampoline(0x630B50, 0x630B55, TaimatuExec_r, false);
 		WriteData<5>(reinterpret_cast<void*>(0x630B32), 0x90); // nop sound queue registering
+	}
+
+	if (config->getBool("Limits", "SetList", true))
+	{
+		WriteData(reinterpret_cast<SETObjectData**>(0x488652), &SetTable[0]); //LOADSET_1P
+		WriteData(reinterpret_cast<SETObjectData**>(0x488424), &SetTable[0]); //LOADSET_2P
+		WriteData(reinterpret_cast<SETObjectData**>(0x488960), &SetTable[0]);
+		WriteData(reinterpret_cast<SETObjectData**>(0x488A23), &SetTable[0]);
+		WriteData(reinterpret_cast<SETObjectData**>(0x4889BD), &SetTable[0]);
+		WriteData(reinterpret_cast<SETObjectData**>(0x488A1F), &SetTable[0]);
+		WriteData(reinterpret_cast<SETObjectData**>(0x488AFA), &SetTable[0]);
+		WriteData(reinterpret_cast<SETObjectData**>(0x488B61), &SetTable[0]);
+		WriteData(reinterpret_cast<SETObjectData**>(0x488BFE), &SetTable[0]);
+		WriteData(reinterpret_cast<SETObjectData**>(0x4C7B7E), &SetTable[0]);
+
+		WriteData(reinterpret_cast<uint32_t*>(0x48894D), SETCount);
+		WriteData(reinterpret_cast<uint32_t*>(0x488952), SETCount);
+		WriteData(reinterpret_cast<uint32_t*>(0x488BEC), SETCount);
+		WriteData(reinterpret_cast<uint32_t*>(0x488BF3), SETCount);
 	}
 }
--- sa2b-limit-break/objects.h
+++ sa2b-limit-break/objects.h
@@ -2,17 +2,33 @@
 
 void Objects_Init(const IniFile* config);
 
-//char __usercall Play3DSound_EntityAndPos@<al>(ObjectMaster* entity@<ebx>, int id@<edi>, NJS_VECTOR* position@<esi>, char volume)
-//static const void* const Play3DSound_EntityAndPosPtr = (void*)0x437420;
-//static inline void Play3DSound_EntityAndPos(ObjectMaster* entity, int id, NJS_VECTOR* position, char volume)
-//{
-//	__asm
-//	{
-//		push[volume]
-//		mov esi, [position]
-//		mov edi, [id]
-//		mov ebx, [entity]
-//		call Play3DSound_EntityAndPosPtr
-//		add esp, 4
-//	}
-//}
+//char __usercall Play3DSound_EntityAndPosOnce@<al>(EntityData1 *entity@<ebx>, int id@<edi>, NJS_VECTOR *position@<esi>, char bank, char volume, __int16 idk)
+static const void* const Play3DSound_EntityAndPosOncePtr = (void*)0x437590;
+static inline char Play3DSound_EntityAndPosOnce(EntityData1* entity, int id, NJS_VECTOR* pos, char bank, char volume, __int16 idk)
+{
+	char result;
+	__asm
+	{
+		push[idk]
+		push[volume]
+		push[bank]
+		mov esi, [pos]
+		mov edi, [id]
+		mov ebx, [entity]
+		call Play3DSound_EntityAndPosPtr
+		mov result, al
+		add esp, 12
+	}
+	return result;
+}
+
+//void __usercall DeleteSoundsOfObject(EntityData1* data@<edx>)
+static const void* const DeleteSoundsOfObjectPtr = (void*)0x437E10;
+static inline void DeleteSoundsOfObject(EntityData1* entity)
+{
+	__asm
+	{
+		mov edx, [entity]
+		call DeleteSoundsOfObjectPtr
+	}
+}
