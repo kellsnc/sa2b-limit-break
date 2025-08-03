9f13d0ddd05180a5b4d05440389101c537c5add3
Kell <kell1151212@gmail.com>
2025-08-03 04:49:18 +0200

    Fix set expansion

--- sa2b-limit-break/set.cpp
+++ sa2b-limit-break/set.cpp
@@ -4,31 +4,27 @@
 #include "FastFunctionHook.hpp"
 #include "set.h"
 
-#ifdef _DEBUG
 static constexpr uint32_t SETCount = 4096;
 static SETObjectData SetTable[SETCount]{};
-#endif
 
 void Set_Init(const IniFile* config)
 {
-#ifdef _DEBUG
 	if (config->getBool("Limits", "SetList", true))
 	{
 		WriteData(reinterpret_cast<SETObjectData**>(0x488652), &SetTable[0]); //LOADSET_1P
 		WriteData(reinterpret_cast<SETObjectData**>(0x488424), &SetTable[0]); //LOADSET_2P
 		WriteData(reinterpret_cast<SETObjectData**>(0x488960), &SetTable[0]);
 		WriteData(reinterpret_cast<SETObjectData**>(0x488A23), &SetTable[0]);
-		WriteData(reinterpret_cast<SETObjectData**>(0x4889BD), &SetTable[0]);
-		WriteData(reinterpret_cast<SETObjectData**>(0x488A1F), &SetTable[0]);
-		WriteData(reinterpret_cast<SETObjectData**>(0x488AFA), &SetTable[0]);
-		WriteData(reinterpret_cast<SETObjectData**>(0x488B61), &SetTable[0]);
-		WriteData(reinterpret_cast<SETObjectData**>(0x488BFE), &SetTable[0]);
-		WriteData(reinterpret_cast<SETObjectData**>(0x4C7B7E), &SetTable[0]);
+		WriteData(reinterpret_cast<short**>(0x4889BD), &SetTable[0].Flags);
+		WriteData(reinterpret_cast<short**>(0x488A1F), &SetTable[0].Flags);
+		WriteData(reinterpret_cast<short**>(0x488AFA), &SetTable[0].Flags);
+		WriteData(reinterpret_cast<short**>(0x488B61), &SetTable[0].Flags);
+		WriteData(reinterpret_cast<short**>(0x488BFE), &SetTable[0].Flags);
+		WriteData(reinterpret_cast<short**>(0x4C7B7E), &SetTable[0].Flags);
 
 		WriteData(reinterpret_cast<uint32_t*>(0x48894D), SETCount);
 		WriteData(reinterpret_cast<uint32_t*>(0x488952), SETCount);
 		WriteData(reinterpret_cast<uint32_t*>(0x488BEC), SETCount);
 		WriteData(reinterpret_cast<uint32_t*>(0x488BF3), SETCount);
 	}
-#endif
 }
